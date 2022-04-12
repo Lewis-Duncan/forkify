42c0ef88f02cf22e29e7c4b46be7b9276115474b
Lewis-Duncan <lewisduncan81@gmail.com>
2022-04-12 23:59:12 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '86c95ec1-40bb-4bd7-800e-36765c711ff1';
+export const MODAL_CLOSE_SEC = 2.5;
